80e2b6aaaee90364b252fcc529c133ba391723fd
Hamish Ivey-Law <hamish@ivey-law.name>
2019-05-22 11:52:42 +1000

    Encode right matrix directly from array.

--- encrypt.cc
+++ encrypt.cc
@@ -56,24 +56,36 @@ struct repeat {
     }
 };
 
+/*
+ * colmat is an array of nrows*ncols int64_t values. It is interpreted
+ * in COLUMN-MAJOR order, that is column 0 is {colmat[0], ..., colmat[nrows-1]},
+ * and row 0 is {colmat[0], colmat[ncols], ..., colmat[ncols * (nrows-1)]}
+ */
 vector<Plaintext>
-encode_right_matrix(const vector<CLK> &clks, BatchEncoder &encoder) {
-    assert(clks.size() > 0);
-    int clksz = clks[0].size();
-    CLK empty_clk(clksz, 0);
+encode_right_matrix(const int64_t *colmat, size_t nrows, size_t ncols, BatchEncoder &encoder)
+{
+    assert(nrows > 0 && ncols > 0);
 
-    int half_slot_count = encoder.slot_count() / 2;
-    assert(half_slot_count % clksz == 0);
-    int clks_per_col = half_slot_count / clksz;
+    size_t half_slot_count = encoder.slot_count() / 2;
+    // TODO: Relax this restriction
+    assert(half_slot_count % nrows == 0);
+    // How many columnt repetitions must we do to fill the plaintext:
+    size_t reps_per_col = half_slot_count / nrows;
 
-    size_t nptxts = (clks.size() + 1) / 2; // ceil(clks.size() / 2.0)
+    size_t i = 0, nptxts = (ncols + 1) / 2; // ceil(ncols / 2.0)
     vector<Plaintext> ptxts(nptxts);
-    for (size_t i = 0, j = 0; j < clks.size(); ++i, j += 2) {
-        vector<int64_t> col(2 * clksz * clks_per_col);
-        vector<int64_t>::iterator halfway = col.begin() + clksz * clks_per_col;
-        std::generate(col.begin(), halfway, repeat(clks[j].begin(), clks[j].end()));
-        std::generate(halfway, col.end(), repeat(clks[j + 1].begin(), clks[j+1].end()));
-        encoder.encode(col, ptxts[i]);
+
+    // Iterate over the columns of colmat, two at a time.
+    vector<int64_t> ptxt(2 * nrows * reps_per_col);
+    auto halfway = ptxt.begin() + nrows * reps_per_col;
+    auto end = colmat + nrows * ncols;
+    for (auto col = colmat; col < end; col += 2*nrows, ++i) {
+        auto col1 = col, col2 = col1 + nrows;
+
+        std::generate(ptxt.begin(), halfway, repeat(col1, col1 + nrows));
+        std::generate(halfway, ptxt.end(), repeat(col2, col2 + nrows));
+
+        encoder.encode(ptxt, ptxts[i]);
     }
     // FIXME: Handle last CLK
     return ptxts;
@@ -126,10 +138,10 @@ void
 seclink_encrypt_left(
     const seclink_ctx_t ctx,
     seclink_emat_t *outmat,
-    const int64_t *inmat, size_t nrows, size_t ncols,
+    const int64_t *rowmat, size_t nrows, size_t ncols,
     const char *pubkey, size_t pubkeybytes)
 {
-    std::vector<CLK> clks = make_clks(inmat, nrows, ncols);
+    std::vector<CLK> clks = make_clks(rowmat, nrows, ncols);
     auto ptxts = encode_left_matrix(clks, ctx->encoder);
     *outmat = new seclink_emat;
     (*outmat)->data = encrypt_all(ctx, ptxts, pubkey, pubkeybytes);
@@ -140,12 +152,11 @@ void
 seclink_encrypt_right(
     const seclink_ctx_t ctx,
     seclink_emat_t *outmat,
-    const int64_t *inmat, size_t nrows, size_t ncols,
+    const int64_t *colmat, size_t nrows, size_t ncols,
     const char *pubkey, size_t pubkeybytes)
 {
     // TODO: Double-check: why do we have to switch nrows and ncols here?
-    std::vector<CLK> clks = make_clks(inmat, ncols, nrows);
-    auto ptxts = encode_right_matrix(clks, ctx->encoder);
+    auto ptxts = encode_right_matrix(colmat, nrows, ncols, ctx->encoder);
     *outmat = new seclink_emat;
     (*outmat)->data = encrypt_all(ctx, ptxts, pubkey, pubkeybytes);
 }
