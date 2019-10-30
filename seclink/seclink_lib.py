a2e5726474af7da75167bd0579fe41f683aca872
Brian Thorne <brian@thorne.link>
2019-10-30 16:31:49 +1100

    Minor cleanup of seclink_lib
    Remove semicolons from the python lib

--- seclink/seclink_lib.py
+++ seclink/seclink_lib.py
@@ -6,18 +6,19 @@ from seclink._libseclink import ffi, lib
 
 lib = ffi.dlopen(None)
 
-def create_ctx(poldeg = 4096, plain_mod = 40961):
+
+def create_ctx(poldeg=4096, plain_mod=40961):
     def clear_ctx(ctx):
         lib.seclink_clear_ctx(ctx[0])
 
-    assert plain_mod % (2*poldeg) == 1, 'plain modulus must = 1 (mod 2n)'
+    assert plain_mod % (2 * poldeg) == 1, 'plain modulus must = 1 (mod 2n)'
     ctx = ffi.new('seclink_ctx_t *')
     lib.seclink_init_ctx(ctx, poldeg, plain_mod, ffi.NULL)
     weakref.finalize(ctx, clear_ctx, ctx)
 
     # See https://cffi.readthedocs.io/en/latest/using.html for why we
     # need to use "subscript 0".
-    return ctx[0];
+    return ctx[0]
 
 
 def keygen(ctx):
@@ -26,10 +27,14 @@ def keygen(ctx):
     def key_to_bytes(carr, nbytes):
         return b''.join(carr[0][j] for j in range(nbytes[0]))
 
-    pkey = ffi.new('char **'); plen = ffi.new('size_t *')
-    skey = ffi.new('char **'); slen = ffi.new('size_t *')
-    gkey = ffi.new('char **'); glen = ffi.new('size_t *')
-    rkey = ffi.new('char **'); rlen = ffi.new('size_t *')
+    pkey = ffi.new('char **');
+    plen = ffi.new('size_t *')
+    skey = ffi.new('char **');
+    slen = ffi.new('size_t *')
+    gkey = ffi.new('char **');
+    glen = ffi.new('size_t *')
+    rkey = ffi.new('char **');
+    rlen = ffi.new('size_t *')
 
     lib.seclink_keygen(ctx, pkey, plen, skey, slen, gkey, glen, rkey, rlen)
 
@@ -63,19 +68,19 @@ def _encrypt_matrix(ctx, mat, pkey, encrypt_fn):
     pkey_buf = ffi.from_buffer(pkey)
 
     emat = ffi.new('seclink_emat_t *')
-    encrypt_fn(ctx, emat, mat_p, nrows, ncols, pkey_buf, len(pkey));
+    encrypt_fn(ctx, emat, mat_p, nrows, ncols, pkey_buf, len(pkey))
     weakref.finalize(emat, _clear_emat, emat)
 
-    return emat[0];
+    return emat[0]
 
 
 def encrypt_left(ctx, mat, pkey):
-    mat = mat.reshape(mat.shape, order='C') # Ensure matrix is row-major
+    mat = mat.reshape(mat.shape, order='C')  # Ensure matrix is row-major
     return _encrypt_matrix(ctx, mat, pkey, lib.seclink_encrypt_left)
 
 
 def encrypt_right(ctx, mat, pkey):
-    mat = mat.reshape(mat.shape, order='F') # Ensure matrix is column-major
+    mat = mat.reshape(mat.shape, order='F')  # Ensure matrix is column-major
     return _encrypt_matrix(ctx, mat, pkey, lib.seclink_encrypt_right)
 
 
@@ -87,6 +92,7 @@ def matmul(ctx, lmat, rmat, gkeys):
 
     return prod[0]
 
+
 def _emat_shape(emat):
     nrows = ffi.new('size_t *')
     ncols = ffi.new('size_t *')
@@ -96,7 +102,7 @@ def _emat_shape(emat):
 
 def decrypt(ctx, inmat, skey):
     nrows, ncols = _emat_shape(inmat)
-    outmat = np.empty(shape = (nrows, ncols), dtype = np.int64)
+    outmat = np.empty(shape=(nrows, ncols), dtype=np.int64)
     outmat_data, ro_flag = outmat.__array_interface__['data']
     assert outmat_data is not None
     assert ro_flag is False
@@ -109,19 +115,19 @@ def decrypt(ctx, inmat, skey):
 
 
 def run_test(
-        left_rows = 2048, # maximum allowable at the moment
-        left_cols = 512,  # 'BF length'
-        right_cols = 2,
-        maxval = 2,       # maximum vector element value + 1; 2 => bit vectors
-        log = lambda *args, **kwargs: None):
+        left_rows=2048,  # maximum allowable at the moment
+        left_cols=512,  # 'BF length'
+        right_cols=2,
+        maxval=2,  # maximum vector element value + 1; 2 => bit vectors
+        log=lambda *args, **kwargs: None):
     assert left_rows > 0 and left_cols > 0 and right_cols > 0, 'dimensions must be positive'
     right_rows = left_cols
 
-    poldeg = 4096      # must be 4096 or 2048
+    poldeg = 4096  # must be 4096 or 2048
     plain_mod = 40961  # must be = 1 (mod 2*poldeg) and < 2^60
 
     # These restriction will be lifted eventually
-    assert left_rows <= poldeg/2 and left_cols <= poldeg/2, 'dimensions too big'
+    assert left_rows <= poldeg / 2 and left_cols <= poldeg / 2, 'dimensions too big'
     if left_cols & (left_cols - 1) != 0:
         print('WARN: left_cols/right_rows sometimes fails if not = 2^n for some n')
 
@@ -142,12 +148,12 @@ def run_test(
 
     # left is a random matrix with shape left_rows x left_cols
     left_size = left_rows * left_cols
-    left = np.random.randint(maxval, size = left_size, dtype=np.int64)
+    left = np.random.randint(maxval, size=left_size, dtype=np.int64)
     left = left.reshape(left_rows, left_cols)
 
     # right is a random matrix with shape left_cols x right_cols
     right_size = right_rows * right_cols
-    right = np.random.randint(maxval, size = right_size, dtype=np.int64)
+    right = np.random.randint(maxval, size=right_size, dtype=np.int64)
     right = right.reshape(right_rows, right_cols, order='F')
 
     log_('encrypting {}x{} left matrix... '.format(left_rows, left_cols))
@@ -176,6 +182,6 @@ def run_test(
     log('result is correct? ', okay)
 
     if not okay:
-        log([(i,j) for i in range(left_rows) for j in range(right_cols) if not result[i][j]])
+        log([(i, j) for i in range(left_rows) for j in range(right_cols) if not result[i][j]])
 
     return okay
